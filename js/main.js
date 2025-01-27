50c3fea6badd921b17721d67f3c4d0f42edfdd31
titanxrio <Titan150208@gmail.com>
2025-01-27 16:02:55 +0100

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Titan Script – All hail the Titan!
-//
-// Hier können Animationen, Fetch-Requests oder Interaktionen platziert werden.
-// Z. B. ein Klick-Event für den CTA-Button:
-
-document.querySelector(".cta-button").addEventListener("click", () => {
-  alert("Titan greift zu! Du bist bereit, tiefer in die Domain einzusteigen.");
-  // Hier könntest du auf eine andere Seite navigieren oder ein Modal öffnen.
-});
